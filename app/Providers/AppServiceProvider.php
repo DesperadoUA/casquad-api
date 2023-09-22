3994725faa8063baec09c5a8664bfe9a1c720e1c
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2023-09-22 21:11:40 +0300

    add migration currency, payment, language

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,9 @@
 <?php
-
+ 
 namespace App\Providers;
-
+ 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
+ 
     /**
      * Bootstrap any application services.
      *
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
