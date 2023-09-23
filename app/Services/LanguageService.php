f5d8ff819e452a3bccc5fdec2ab80bd153ae8c2b
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2023-09-23 09:00:32 +0300

    add currency post type

--- app/Services/LanguageService.php
+++ app/Services/LanguageService.php
@@ -21,7 +21,6 @@ class LanguageService extends FrontBaseService {
     public function show($id) {
         $post = new Posts(['table' => $this->configTables['table'], 'table_meta' => $this->configTables['table_meta']]);
         $data = $post->getPublicPostByUrl($id);
-
         if(!$data->isEmpty()) {
             $this->response['body'] = $this->serialize->frontSerialize($data[0], $this->shemas);
 
